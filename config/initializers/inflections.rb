ad38fdf1b5217a0db4d7bf51a786d9d149f41bd4
ViniciusAbreu00 <vinicius.abreu@nolifretes.com.br>
2022-06-13 10:23:56 -0300

    estruturando o projeto

---
+++ config/initializers/inflections.rb
@@ -0,0 +1,13 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.plural /^(ox)$/i, "\\1en"
+#   inflect.singular /^(ox)en/i, "\\1"
+#   inflect.irregular "person", "people"
+#   inflect.uncountable %w( fish sheep )
+# end
+
+# These inflection rules are supported but not enabled by default:
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.acronym "RESTful"
+# end
